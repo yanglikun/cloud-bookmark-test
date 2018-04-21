8cfea81f783e5ed0cb1b60bb8a2d87911c1d1cfa
yanglikun <yanglikun@jd.com>
2018-04-22 01:35:43 +0800

    初始化

---
+++ src/com/github/yanglikun/cloudbookmark/OrderService.java
@@ -0,0 +1,13 @@
+package com.github.yanglikun.cloudbookmark;
+
+public class OrderService {
+
+    public void startOrder() {
+        System.out.println("开始下单");
+    }
+
+
+    public void endOrder() {
+        System.out.println("结束下单");
+    }
+}
---
+++ src/com/github/yanglikun/cloudbookmark/PayService.java
@@ -0,0 +1,14 @@
+package com.github.yanglikun.cloudbookmark;
+
+public class PayService {
+
+    public void startPay() {
+        System.out.println("开始支付");
+    }
+
+
+    public void endPay() {
+        System.out.println("结束支付");
+    }
+
+}
